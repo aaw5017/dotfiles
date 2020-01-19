ab5938c392ce2c40cd04201fff0389bf68465692
Alex Wardi <aaw5017@gmail.com>
2020-01-18 19:35:04 -0500

    Massive cleanup - Never use any of this stuff, no need to keep around

--- user-aliases.cmd
+++ user-aliases.cmd
@@ -5,7 +5,7 @@
 ;= goto:eof
 ;= Add aliases below here
 e.=explorer .
-gl=git log --oneline --all --graph --decorate  $*
+gl=git log --oneline --all --graph --decorate $*
 ls=ls --show-control-chars -F --color $*
 pwd=cd
 clear=cls
@@ -39,4 +39,4 @@ cdatlas=cd c:\dev\Elysium\SocialPandas\AtlasStack
 gbdone=git rev-parse --abbrev-ref HEAD >branchname.txt && set /P bname=<branchname.txt && del branchname.txt && git checkout dev && git pull origin dev && call git branch -d ^%bname^%
 gstashu=git stash -u
 cdwamp=cd c:\inetpub\wamp\www
-chrome="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+chrome="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" $*
